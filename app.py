52c352b215adc8e462dbdab84e439f992602b6b8
SrikarMudili <66754140+SrikarMudili@users.noreply.github.com>
2020-07-06 18:50:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import jsonify
+from flask_cors import CORS
 
 # creates a Flask application, named app
 app = Flask(__name__)
+CORS(app)
 
 # a route where we will display a welcome message via an HTML template
 @app.route("/resume")
