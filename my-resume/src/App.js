7c614364139a7af7f6395f7f39cccfeadc3310de
Srikar Mudili <srikarm@Srikars-MacBook-Pro.local>
2020-07-10 18:43:39 -0500

    more progress

--- my-resume/src/App.js
+++ my-resume/src/App.js
@@ -32,28 +32,28 @@ class App extends React.Component {
           this.state.resume == null ? (<h4> Resume is loading </h4>) : (<div>
 
             
-            <h1> Hello, my name is {this.state.resume.name}. </h1>
-            <h3> I am {this.state.resume.age} years old! </h3>
-            <h3> I got a {this.state.resume.gpa} out of 4.3 GPA!</h3>
-            <h2> This is my Resume ꜜꜜꜜꜜꜜꜜ</h2>
+            <h1 className="bg-danger"> Hello, my name is {this.state.resume.name}. </h1>
+            <h3 className="bg-success"> I am {this.state.resume.age} years old! </h3>
+            <h3 className="bg-info"> I got a {this.state.resume.gpa} out of 4.3 GPA!</h3>
+            <h2 className="bg-warning"> This is my Resume ꜜꜜꜜꜜꜜꜜ</h2>
 
-            <h3> HOBBIES: </h3>
-            <ul className="list-group">
+            <h3 className="bg-danger"> HOBBIES: </h3>
+            <ul className="bg-danger">
               {this.state.resume.hobbies.map(hobby => <li className='list-group-item'>{hobby}</li>)}
             </ul>
 
-            <h3> SPORTS: </h3>
-            <ul className="list-group">
+            <h3 className="bg-success"> SPORTS: </h3>
+            <ul className="bg-success">
               {this.state.resume.sports.map(sport => <li className='list-group-item'>{sport}</li>)}
             </ul>
 
-            <h3> COURSES I HAVE TAKEN: </h3>
-            <ul className="list-group">
+            <h3 className="bg-info"> COURSES I HAVE TAKEN: </h3>
+            <ul className="bg-info">
               {this.state.resume.courses.map(course => <li className='list-group-item'>{course}</li>)}
             </ul>
 
-            <h3> AWARDS: </h3>
-            <ul className="list-group">
+            <h3 className="bg-warning"> AWARDS: </h3>
+            <ul className="bg-warning">
               {this.state.resume.awards.map(award => <li className='list-group-item'>{award}</li>)}
             </ul>
 
